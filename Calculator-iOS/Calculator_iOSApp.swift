5abfee810690ea7d0ab3a566d80c5abe7a65c0f6
Filip S <filip.syn@protonmail.com>
2023-03-19 15:20:34 +0100

    Initial Commit

---
+++ Calculator-iOS/Calculator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_iOSApp.swift
+//  Calculator-iOS
+//
+//  Created by Filda on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
